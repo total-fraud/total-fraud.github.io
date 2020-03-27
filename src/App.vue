adec6bad7f6b4b2f06a1eb3f46b8f10c6ac2005a
total-fraud <lumen437@gmail.com>
2020-03-27 16:22:09 +0400

    replaced normal version with vue one from sandbox

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
-    <Header/>
-    <Feed/>
-    <Aside/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Header from "./components/Header";
-import Feed from "./components/Feed";
-import Aside from "./components/Aside";
-export default {
-  name: "App",
-  components: {
-    Header,
-    Feed,
-    Aside
-  }
-};
-</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
- <style>
-@import url("./css/container-style.css");
-@import url("https://fonts.googleapis.com/css?family=Bungee+Shade|Oswald:400,500,600|Roboto:500&display=swap&subset=cyrillic");
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
